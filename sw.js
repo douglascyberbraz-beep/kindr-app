2b1c86610cc2b85711e4e2dd04022cf9e8aaa267
douglascyberbraz-beep <douglas.cyberbraz@gmail.com>
2026-02-15 12:27:00 +0100

    Fix: Service Worker paths for GitHub Pages sub-directory

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
 const CACHE_NAME = 'kindr-cache-v1';
 const ASSETS = [
-    '/',
-    '/index.html',
-    '/css/main.css',
-    '/js/app.js',
-    '/js/services/data.js',
-    '/js/services/auth.js',
-    '/js/pages/map.js',
-    '/assets/logo.png',
-    '/assets/logo.svg',
+    './',
+    'index.html',
+    'css/main.css',
+    'js/app.js',
+    'js/services/data.js',
+    'js/services/auth.js',
+    'js/pages/map.js',
+    'assets/logo.png',
+    'assets/logo.svg',
     'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
     'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
 ];
